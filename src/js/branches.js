742d6cdb856a966982ca0fe193b8593c058f843d
Prince Peñaflor <princepenaflor61@gmail.com>
2026-07-13 15:58:13 +0800

    resolve 404 error for image assets

--- src/js/branches.js
+++ src/js/branches.js
@@ -5,7 +5,7 @@ const branches = {
 
     manila:{
         title:"Manila Branch",
-        image:"public/branch1.png",
+        image:"/branch1.png",
         description:"Our main office serves as the central hub for recruitment, applicant processing, and deployment coordination. Visit us for application assistance, document submission, and inquiries.",
         address:"Suite 305, 3rd Floor, AP Building, Pedro Gil corner F, Agoncillo St, Ermita, Manila, 1000",
         phones:["+63 916 3992 066 (Globe)", "+63 962 938 5411 (Smart)", "+63 63 302 3068 (Landline)"],
@@ -14,7 +14,7 @@ const branches = {
 
     cagayan:{
         title:"Cagayan De Oro City",
-        image:"public/branch2.png",
+        image:"/branch2.png",
         description:"provides trusted recruitment services, application assistance, and documentation support for aspiring Overseas Filipino Workers throughout the Cagayan Valley region. Our team is dedicated to making your overseas employment journey smooth and successful.",
         address:"No. 82 Door C Gordiel Bldg., Corrales Ave., corner J. R. Borja Street, Cagayan De Oro City",
         phones:["+63 916 3992 066 (Globe)", "+63 962 938 5411 (Smart)", "+63 63 302 3068 (Landline)"],
@@ -23,7 +23,7 @@ const branches = {
 
     lanao:{
         title:"Lanao Del Norte",
-        image:"public/branch3.png",
+        image:"/branch3.png",
         description:"Committed to serving applicants in Northern Mindanao by providing trusted recruitment services, application assistance, document processing, and overseas employment guidance. We strive to make every applicant's journey toward working abroad smooth, efficient, and successful.",
         address:"Tubod Lanao Branch: Stall no.2, 2F, AAQ Bldg., Crossing Tubod, Lanao Del Norte",
         phones:["+63 916 3992 066 (Globe)", "+63 962 938 5411 (Smart)", "+63 63 302 3068 (Landline)"],
@@ -32,7 +32,7 @@ const branches = {
 
     agusan:{
         title:"Agusan Del Sur",
-        image:"public/branch4.png",
+        image:"/branch4.png",
         description:"Dedicated to assisting aspiring Overseas Filipino Workers throughout Agusan del Sur and neighboring communities. We provide reliable recruitment services, application assistance, document processing, and pre-deployment guidance to help applicants achieve successful overseas careers with confidence.",
         address:"Bayugan Agusan Del Sur: 2F, QuireQuire Bldg., P-3, Taglatawan, Bayugan City, Agusan Del Sur",
          phones:["+63 916 3992 066 (Globe)", "+63 962 938 5411 (Smart)", "+63 63 302 3068 (Landline)"],
@@ -41,7 +41,7 @@ const branches = {
 
     cebu:{
         title:"Talisay City, Cebu",
-        image:"public/branch5.png",
+        image:"/branch5.png",
         description:"proudly serves job seekers across Cebu and the Visayas region by offering professional recruitment services, application support, document processing, and overseas employment consultation. Our experienced team is committed to guiding every applicant through a smooth and successful deployment process.",
         address:"Talisay, Cebu: Dor #2 and #3, 2F Gee Ann Square Mall, Nonoc Tabunok, Talisay City, Cebu",
          phones:["+63 916 3992 066 (Globe)", "+63 962 938 5411 (Smart)", "+63 63 302 3068 (Landline)"],
